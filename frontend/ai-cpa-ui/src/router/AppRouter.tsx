ba17896c5c4e43df4b06a0fc42bae3138fa1ab08
Rahul Chauhan <rchauhan9102@gmail.com>
2025-11-21 02:20:19 +0530

    Create Login Page

--- frontend/ai-cpa-ui/src/router/AppRouter.tsx
+++ frontend/ai-cpa-ui/src/router/AppRouter.tsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "@/pages/home";
-import About from "@/pages/about";
+import Home from "@/pages/home/Home.tsx";
+import About from "@/pages/about/About.tsx";
+import Login from "@/pages/login/Login.tsx";
 
+
+// <Route path="/" element={<Home />} />
+// <Route path="/about" element={<About />} />
 const AppRouter = () => (
   <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/about" element={<About />} />
+    <Route path="/" element={<Login />} />
   </Routes>
 );
 
